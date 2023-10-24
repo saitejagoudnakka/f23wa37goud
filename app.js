71dfd0657d0e23633db79049a6b559f33d37cc33
Sai Teja Goud Nakka <S565437@nwmissouri.edu>
2023-10-23 22:08:29 -0500

    added computation

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var mydataRouter = require('./routes/mydata');
 
 var usersRouter = require('./routes/users');
+var computationRouter = require('./routes/computation');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydataRouter);
+app.use('/computation', computationRouter );
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
