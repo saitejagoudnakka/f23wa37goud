f4a8025a7f3c61945143d2e10cef8df6e75330b7
Sai Teja Goud Nakka <S565437@nwmissouri.edu>
2023-10-25 19:44:07 -0500

    Update computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-var random1=Math.round(Math.random()*10);
-var random2=Math.round(Math.random()*20);
-num1= random1*random1;
-num2= random2*random2;
-var result=0;
-
 router.get('/', function(req, res, next) {
-   var result=Math.sqrt(num1+num2);
-  res.send(`random numbers are ${random1} and ${random2} <br>
-  and square root of f(${random1},${random2}) is ${result}`);
+  var x = parseFloat(req.query.x) || Math.round(Math.random()*10);
+var result=0;
+   var result=Math.acosh(x);
+  res.send(`random numbers are ${x} <br>
+  and cosine of a number  f(${x}) is ${result}`);
 });
 
 module.exports = router;
